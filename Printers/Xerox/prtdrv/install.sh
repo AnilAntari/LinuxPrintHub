c4fa7331d9d421500e6795b1ec286f6711b113d1
AnilAntari <86964331+AnilAntari@users.noreply.github.com>
2024-01-25 11:05:44 +0300

    Update install.sh

--- Printers/Xerox/prtdrv/install.sh
+++ Printers/Xerox/prtdrv/install.sh
@@ -11,7 +11,7 @@ apt-get update
 apt-get install sane-airscan
 
 wget https://download.support.xerox.com/pub/drivers/CQ8580/drivers/linux/pt_BR/XeroxOfficev5Pkg-Linuxx86_64-5.20.661.4684.$OS
-apt-get install XeroxOfficev5Pkg-Linuxx86_64-5.20.661.4684.$OS
+apt-get install ./XeroxOfficev5Pkg-Linuxx86_64-5.20.661.4684.$OS
 
 cat  << 'EOF'
 The driver is installed. For further configuration, use the command:
