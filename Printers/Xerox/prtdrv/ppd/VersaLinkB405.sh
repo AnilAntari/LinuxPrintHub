f2051ee0ad1a4d775530317c7b371a22b5c7ff47
AnilAntari <86964331+AnilAntari@users.noreply.github.com>
2024-05-28 20:47:52 +0300

    Update VersaLinkB405.sh

--- Printers/Xerox/prtdrv/ppd/VersaLinkB405.sh
+++ Printers/Xerox/prtdrv/ppd/VersaLinkB405.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ 1000 -ne 0 ] && echo 'need root' && exit 1
+[ $EUID -ne 0 ] && echo 'need root' && exit 1
 [ ! -e /usr/share/ppd/xerox ] && mkdir /usr/share/ppd/xerox
 
 wget https://download.support.xerox.com/pub/drivers/VLB400/drivers/win10/ar/VersaLink_B400_5.662.0.0_PPD_English.exe
