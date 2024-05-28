9f560b9f7e117ab951b5f17f60dbcc7b811916af
AnilAntari <retro201272@gmail.com>
2024-05-28 20:34:00 +0300

    Added a script for installing ppd files for Xerox VersaLink B405

---
+++ Printers/Xerox/prtdrv/ppd/VersaLinkB405.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ 1000 -ne 0 ] && echo 'need root' && exit 1
+[ ! -e /usr/share/ppd/xerox ] && mkdir /usr/share/ppd/xerox
+
+wget https://download.support.xerox.com/pub/drivers/VLB400/drivers/win10/ar/VersaLink_B400_5.662.0.0_PPD_English.exe
+mkdir VersaLinkB405_ppd
+unrar x VersaLink_B400_5.662.0.0_PPD_English.exe r *.ppd o VersaLinkB405_ppd   -y
+cp VersaLinkB405_ppd/*.ppd /usr/share/ppd/xerox
