81e322288d4ed1f67443838b12742c9645eec5c5
AnilAntari <86964331+AnilAntari@users.noreply.github.com>
2024-09-03 14:41:06 +0300

    Update install.sh

--- Printers/Pantum/install.sh
+++ Printers/Pantum/install.sh
@@ -6,7 +6,7 @@ OS=''
 [ -e /etc/altlinux-release ] && OS=rpm
 [ -z OS ] && echo 'only alt supported' && exit 2
 	
-elif [ $OS == "rpm" ]; then
+if [ $OS == "rpm" ]; then
 	wget http://altrepo.ru/local-p10/x86_64/RPMS.local-p10/pantum-106-1.1.106-alt1.x86_64.rpm
 	apt-get install ./pantum-106-1.1.106-alt1.x86_64.rpm system-config-printer -y
 fi
